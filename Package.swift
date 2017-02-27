9512fb26549f40314581007777d98324c555995e
Grigory Serebryanyy <gs@messapps.com>
2017-02-27 06:56:54 +0300

    add SlackKit dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "mma-team-bot"
+    name: "mma-team-bot",
+    dependencies: [
+        .Package(url: "https://github.com/pvzig/SlackKit.git", majorVersion: 0, minor: 0),
+        ]
 )
